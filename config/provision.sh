a6419a4e2ad6979f17c5e111c020a58d726a3718
nickmorales <nmorales3142@gmail.com>
2018-12-11 10:57:09 -0500

    install libssl-dev

--- config/provision.sh
+++ config/provision.sh
@@ -442,7 +442,7 @@
 	fi
 
 	#Install R
-	sudo apt-get install apt-transport-https -y
+	sudo apt-get install apt-transport-https libssl-dev -y
 	sudo apt-get install dirmngr --install-recommends -y
 	sudo sed -i "\$adeb https://cran.cnr.berkeley.edu/bin/linux/debian stretch-cran35/" /etc/apt/sources.list
 	sudo apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
